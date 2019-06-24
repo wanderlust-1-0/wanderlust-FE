4fdd057f518c85bbc594800ebd86d27f47d621c2
Jeff Oliver <jeffoliver3384@gmail.com>
2019-06-24 16:20:45 -0400

    created signin method so user can grap auth token and will direct them to the dashboard or explore tours views

--- wanderlust/src/actions/index.js
+++ wanderlust/src/actions/index.js
@@ -1,22 +1,39 @@
-import Axios from 'axios';
+import axios from 'axios';
+
+export const SIGNUP_FETCHING = 'SIGNUP_FETCHING';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+
+export const signUp = accountData => dispatch => {
+  dispatch({ type: SIGNUP_FETCHING });
+  return axios
+    .post('https://wunderlust-ac056.firebaseio.com/users.json', accountData)
+    .then(response => {
+      console.log('RESPONSE', response.config.data);
+      // localStorage.setItem('authentication_token', 'loggedIn');
+      // /* dispatch({ type: SIGNUP_SUCCESS, payload: response.data }); */
+      dispatch({ type: SIGNUP_SUCCESS, payload: response.config.data });
+    })
+    .catch(error => {
+      dispatch({ type: SIGNUP_FAILURE, payload: 'Signin failed.' });
+    });
+};
 
 export const SIGNIN_FETCHING = 'SIGNIN_FETCHING';
 export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
 export const SIGNIN_FAILURE = 'SIGNIN_FAILURE';
 
-export const signin = accountData => dispatch => {
+export const signin = () => dispatch => {
   dispatch({ type: SIGNIN_FETCHING });
-  return Axios.post(
-    'https://wunderlust-ac056.firebaseio.com/users.json',
-    accountData,
-  )
-    .then(response => {
-      console.log('RESPONSE', response.config.data);
-      localStorage.setItem('authentication_token', 'loggedIn');
-      /* dispatch({ type: SIGNIN_SUCCESS, payload: response.data }); */
-      dispatch({ type: SIGNIN_SUCCESS, payload: response.config.data });
+  return axios
+    .get('https://wunderlust-ac056.firebaseio.com/authentication_token.json')
+    .then(res => {
+      console.log('token response: ', res.data);
+      localStorage.setItem('auth-token', res.data);
+      dispatch({ type: SIGNIN_SUCCESS, payload: res.data });
     })
-    .catch(error => {
-      dispatch({ type: SIGNIN_FAILURE, payload: 'Signin failed.' });
+    .catch(err => {
+      console.log('token err: ', 'token err');
+      dispatch({ type: SIGNIN_FAILURE, payload: 'token err' });
     });
 };
