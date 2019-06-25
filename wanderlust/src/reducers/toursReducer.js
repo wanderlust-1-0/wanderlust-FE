aa6582b4258ca06b15b47e516310ab164c461aca
Jeff Oliver <jeffoliver3384@gmail.com>
2019-06-25 13:30:46 -0400

    built out the rest of the reducer for the tours

--- wanderlust/src/reducers/toursReducer.js
+++ wanderlust/src/reducers/toursReducer.js
@@ -1,3 +1,23 @@
+import {
+  FETCHING_TOURS_START,
+  FETCHING_TOURS_SUCCESS,
+  FETCHING_TOURS_FAILURE,
+} from '../actions';
+
+import { ADD_TOUR_START, ADD_TOUR_SUCCESS, ADD_TOUR_FAILURE } from '../actions';
+
+import {
+  UPDATE_TOUR_START,
+  UPDATE_TOUR_SUCCESS,
+  UPDATE_TOUR_FAILURE,
+} from '../actions';
+
+import {
+  DELETE_TOUR_START,
+  DELETE_TOUR_SUCCESS,
+  DELETE_TOUR_FAILURE,
+} from '../actions';
+
 const initialState = {
   tours: [],
   fetchingTours: false,
@@ -6,7 +26,90 @@ const initialState = {
   addingTour: false,
   addingTourErr: '',
   updatingTour: false,
-  updatingTourErr: ''
-}
+  updatingTourErr: '',
+  deletingTour: false,
+  deletingTourErr: '',
+};
 
-export const tourReducer
+export const tourReducer = (state = initialState, action) => dispatch => {
+  switch (action.type) {
+    case FETCHING_TOURS_START:
+      return {
+        ...state,
+        fetchingTours: true,
+        fetchingToursErr: '',
+      };
+    case FETCHING_TOURS_SUCCESS:
+      return {
+        ...state,
+        tours: action.payload,
+        fetchingTours: false,
+        fetchingToursErr: '',
+      };
+    case FETCHING_TOURS_FAILURE:
+      return {
+        ...state,
+        fetchingTours: false,
+        fetchingToursErr: action.payload,
+      };
+    case ADD_TOUR_START:
+      return {
+        ...state,
+        addingTour: true,
+        addingTourErr: '',
+      };
+    case ADD_TOUR_SUCCESS:
+      return {
+        ...state,
+        tours: action.payload,
+        addingTour: false,
+        addingTourErr: '',
+      };
+    case ADD_TOUR_FAILURE:
+      return {
+        ...state,
+        addingTour: false,
+        addingTourErr: action.payload,
+      };
+    case UPDATE_TOUR_START:
+      return {
+        ...state,
+        updatingTour: true,
+        updatingTourErr: '',
+      };
+    case UPDATE_TOUR_SUCCESS:
+      return {
+        ...state,
+        tours: action.payload,
+        updatingTour: false,
+        updatingTourErr: '',
+      };
+    case UPDATE_TOUR_FAILURE:
+      return {
+        ...state,
+        updatingTour: false,
+        updatingTourErr: action.payload,
+      };
+    case DELETE_TOUR_START:
+      return {
+        ...state,
+        deletingTour: true,
+        deletingTourErr: '',
+      };
+    case DELETE_TOUR_SUCCESS:
+      return {
+        ...state,
+        tours: action.payload,
+        deletingTour: false,
+        deletingTourErr: '',
+      };
+    case DELETE_TOUR_FAILURE:
+      return {
+        ...state,
+        deletingTour: false,
+        deletingTourErr: action.payload,
+      };
+    default:
+      return state;
+  }
+};
