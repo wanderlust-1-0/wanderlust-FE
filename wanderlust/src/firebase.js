73f0bc1f6e3ff0f9856049b011a75f260d4409a0
Jeff Oliver <jeffoliver3384@gmail.com>
2020-06-04 20:52:45 -0400

    install firebase for auth, created firebase.js, firebaseConfig and initialized app

---
+++ wanderlust/src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCca7zM4LtiL7PygucWbisKSzAFLpp8Be0",
+  authDomain: "wanderlust-4a7a5.firebaseapp.com",
+  databaseURL: "https://wanderlust-4a7a5.firebaseio.com",
+  projectId: "wanderlust-4a7a5",
+  storageBucket: "wanderlust-4a7a5.appspot.com",
+  messagingSenderId: "420115187470",
+  appId: "1:420115187470:web:ff29e0a7bf3e36b2d1395f",
+  measurementId: "G-TFLQKXX3K7",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const auth = firebase.auth();
